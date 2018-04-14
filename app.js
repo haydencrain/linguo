eb84514f695edd8589110946a2026501c0a745d2
Hayden Crain <haydencrain@optusnet.com.au>
2018-04-14 18:53:35 +1000

    add meme, random and hero usage, remove helpers

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ if (process.version.slice(1).split('.')[0] < 8)
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.config = require('./config.js');
-const helpers = require('./helpers.js');
+// const helpers = require('./helpers.js');
 const colors = require('./colors.js');
 
 // plugins
@@ -18,7 +18,7 @@ const frinkiac = plugins.frinkiac;
 Client actions
 **************************************************************************************************/
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}! Serving ${client.channels.array.length} channels`);
+  console.log(`Logged in as ${client.user.tag}! Handling ${client.guilds.array().length} server(s)`);
   client.user.setActivity('`Linguo help` for help');
 });
   
@@ -38,47 +38,111 @@ client.on('debug', e => { console.info(`${colors.dim}%s${colors.reset}`, e); });
 /**************************************************************************************************
 Command handler
 **************************************************************************************************/
+const commands = [
+  {
+    name: 'help',
+    description: 'Displays available commands and a description of how to use each',
+    process: (message, args) => {
+      
+    }
+  },
+  {
+    name: 'dead?',
+    description: 'Ask Linguo if he\'s dead',
+    process: async (message, args) => { 
+      
+    }
+  },
+  {
+    name: 'say',
+    description: 'Linguo will repeat your message',
+    process: (message, args) => { 
+      
+    }
+  },
+  {
+    name: 'search',
+    description: 'Search for a particular quote from The Simpsons, and Linguo will return the most relevant screencap',
+    process: async (message, args) => {
+      
+    }
+  },
+  {
+    name: 'image',
+    description: 'Enter a particaluar episode number and timestamp, and Linguo will return the specific screencap.\n\te.g. \'S05E08 116632\'',
+    process: async (message, args) => { 
+    
+    }
+  }
+];
+
 client.on('message', async message => {
   // return if message is from bot or is a client message
   if (message.author.id === client.user.id || message.author.bot) return;
 
   // determine which prefix is used, return if message does not contain prefix 
-  var prefix = helpers.determinePrefixOrMentioned(message, client);
+  var prefix = determinePrefixOrMentioned(message, client);
   if (message.content.indexOf(prefix) !== 0) return;
 
   // split into command and args
   var args = message.content.slice(prefix.length).trim().split(/ +/g);
   var command = args.shift();
-
-  var caption, query, search, firstData;
+  
+  var caption;
   switch (command) {
     case 'help':
-      message.channel.send('nah sorry can\'t help you');
+      var help = '``Current available commands:\n\n';
+      commands.forEach((command) => { help += `${command.name}\n\t${command.description}\n\n`; });
+      help += '``';
+      message.channel.send(help)
+        .catch(err => message.channel.send(err));
       break;
-
+    
     case 'dead?':
       var m = await message.channel.send('Linguo dead?');
       m.edit('Linguo IS dead!');
       break;
     
-    case 'say': 
-      message.delete().catch(owo => { message.send(owo);});    
+    case 'say':
+      message.delete().catch(owo => { console.log(owo); });
       message.channel.send(args.join(' '));
       break;
     
     case 'search':
-      query = args.join(' ');
-      search = await frinkiac.search(query);
-      firstData = search.data[0];
+      var response = await frinkiac.search(args.join(' '));
+      var firstData = response.data[0];
       caption = await frinkiac.caption(firstData.Episode, firstData.Timestamp);
-      message.channel.send(caption.data.Subtitles[0].Content, { files: [frinkiac.imageURL(firstData.Episode, firstData.Timestamp)] })
+      message.channel.send(toSubtitleString(caption.data.Subtitles), toImageAttachment(caption))
         .catch(error => message.send(error));
       break;
-
+    
     case 'image':
-      caption = await frinkiac.caption(args[0], args[1]);  
-      message.channel.send(caption.data.Subtitles[0].Content, { files: [frinkiac.imageURL(args[0], args[1])] })
+      caption = await frinkiac.caption(args[0], args[1]);
+      message.channel.send(toSubtitleString(caption.data.Subtitles), toImageAttachment(caption))
         .catch(error => message.send('Image cannot be found! Make sure you have entered both an episode number and timestamp.'));
+      break;
+    
+    case 'random':
+      caption = await frinkiac.random();
+      message.channel.send(toSubtitleString(caption.data.Subtitles), toImageAttachment(caption))
+        .catch(error => message.send(error));
+      break;
+    
+    case 'frinkiac':
+      message.channel.send('', { files: [frinkiac.heroURL()] })
+        .catch(error => message.send(error));
+      break;
+    
+    case 'meme':
+      response = await frinkiac.search(args.join(' '));
+      firstData = response.data[0];
+      caption = await frinkiac.caption(firstData.Episode, firstData.Timestamp);
+      message.channel.send(frinkiac.memeURL(
+        caption.data.Frame.Episode,
+        caption.data.Frame.Timestamp,
+        toSubtitleString(caption.data.Subtitles)
+      ))
+        .catch(error => message.channel.send(error));
       break;
   }
 
@@ -110,5 +174,39 @@ client.on('message', async message => {
   // .addBlankField(true)
   // .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 });
+
+const determinePrefixOrMentioned = (message, client) => {
+  const prefixMention = new RegExp(`<@!?${client.user.id}>`);
+  return message.content.match(prefixMention)
+    ? message.content.match(prefixMention)[0]
+    : client.config.prefix;
+};
+
+const toSubtitleString = (subtitles) => {
+  var subtitleString = '';
+  subtitles.forEach(subtitle => { subtitleString += subtitle.Content + ' \n'; });
+  return subtitleString;
+};
+
+const toImageAttachment = (caption) => {
+  return ({
+    files: [frinkiac.imageURL(
+      caption.data.Frame.Episode,
+      caption.data.Frame.Timestamp
+    )]
+  });
+};
+
+const toMemeAttachment = (caption) => {
+  var memeURL = frinkiac.memeURL(
+    caption.data.Frame.Episode,
+    caption.data.Frame.Timestamp,
+    toSubtitleString(caption.data.Subtitles)
+  );
+  console.log(memeURL);
+  return ({
+    files: [memeURL]
+  });
+};
 
 client.login(client.config.token);
--- helpers.js
+++
@@ -1,7 +0,0 @@
-
-exports.determinePrefixOrMentioned = (message, client) => {
-  const prefixMention = new RegExp(`<@!?${client.user.id}>`)
-  return message.content.match(prefixMention)
-    ? message.content.match(prefixMention)[0]
-    : client.config.prefix;
-}
